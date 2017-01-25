010b9b83fe215d6e138d1cc27b35f78a1c0e28b6
skconan <supakit.kr@gmail.com>
2017-01-25 15:23:38 +0700

    refactoring code

--- getColorPX.m
+++ getColorPX.m
@@ -1,73 +1,59 @@
-clear all;
-cam = webcam('USB2.0 VGA UVC WebCam');
-hmin = 1;
-hmax = 0;
-vmin = 1;
-vmax = 0;
-smin = 1;
-smax = 0;
+% Author : Supakit Kriangkhajorn
+% Follow : skconan
+function getColorPX()
+    cam = webcam('USB2.0 VGA UVC WebCam');
+    hmin = 1;
+    hmax = 0;
+    vmin = 1;
+    vmax = 0;
+    smin = 1;
+    smax = 0;
+    width = 640;
+    height = 480;
+    while true				
+        img = snapshot(cam);
+        hsv = rgb2hsv(img);
 
-while true
-    % Create slider
-    
-				
-    img = snapshot(cam);
-    hsv = rgb2hsv(img);
-%     subplot(1,2,1);
-    figure(1); 
-    imshow(hsv);
-%     preview(cam);
-    
-    [y,x] = ginput(1);
-%     disp(length(x));
-%     disp(x(1));
-    if x(1) < 0 || y(1) < 0
-        continue
-    end
-%     disp('1');
-   
-    x = uint16(x);
-    y = uint16(y);
-    disp(size(hsv));
-    h = hsv(x,y,1);
-    s = hsv(x,y,2); 
-    v = hsv(x,y,3);
-    if h <= hmin
-        hmin = h;
-    end
-    if h >= hmax
-        hmax = h;
-    end
-    if s <= smin
-        smin = s;
-    end
-    if s >= smax
-        smax = s;
-    end
-    if v <= vmin
-        vmin = v;
-    end
-    if v >= vmax
-        vmax = v;
-    end
-    fprintf('==========\n%d %d\n %.2f %.2f %.2f\n %.2f %.2f %.2f\n %.2f %.2f %.2f\n',x,y,h,s,v,hmax,smax,vmax,hmin,smin,vmin);
-    result = inrange_input(hsv,hmin,hmax,smin,smax,vmin,vmax);
-%     subplot(1,2,2);
-    
-    figure(2);
-    imshow(result);
+        figure(1); 
+        imshow(hsv);
 
-    sld = uicontrol('Style', 'slider',...
-        'Min',1,'Max',50,'Value',41,...
-        'Position', [400 20 120 20],...
-        'Callback', @surfzlim); 
-    pause(0.01);
+        [col, row] = ginput(1);
+        disp(row(1));
+        disp(col(1));
+        if ~(0 <= row(1) && row(1) <= height && 0 <= col(1) && col(1) <= width)
+            disp('1');
+            continue
+        end
+
+        row = uint16(row);
+        col = uint16(col);
+
+        h = hsv(row, col, 1);
+        s = hsv(row, col, 2);
+        v = hsv(row, col, 3);
+
+        if h <= hmin
+            hmin = h;
+        end
+        if h >= hmax
+            hmax = h;
+        end
+        if s <= smin
+            smin = s;
+        end
+        if s >= smax
+            smax = s;
+        end
+        if v <= vmin
+            vmin = v;
+        end
+        if v >= vmax
+            vmax = v;
+        end
+
+        fprintf('==========\n%d %d\n %.2f %.2f %.2f\n %.2f %.2f %.2f\n %.2f %.2f %.2f\n',col,row,h,s,v,hmax,smax,vmax,hmin,smin,vmin);
+        result = inrange_input(hsv,hmin,hmax,smin,smax,vmin,vmax);
+        figure(2);
+        imshow(result);
+    end
 end
-
-function surfzlim(source,event)
-        val = 51 - source.Value;
-        % For R2014a and earlier:
-        % val = 51 - get(source,'Value');
-
-        zlim(ax,[-val val]);
-    end
