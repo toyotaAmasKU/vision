c232c6729f2f7c8d4650521c3c399c8dddb5a23e
skconan <supakit.kr@gmail.com>
2017-02-22 19:00:57 +0700

    edit range of color

--- inrange_input.m
+++ inrange_input.m
@@ -1,11 +1,12 @@
 % Author : Supakit Kriangkhajorn
 % Follow me : skconan
+% Add hh variable for range of red color
+
 function y = inrange_input(hsv,hmin,hmax,smin,smax,vmin,vmax)
-    
-    h = hsv(:,:,1) >= hmin & hsv(:,:,1) <= hmax;
-    s = hsv(:,:,2) >= smin & hsv(:,:,2) <= smax;
+    h = hsv(:,:,1) >= 0.88 & hsv(:,:,1) <= hmax;
+    hh = hsv(:,:,1) >= 0 & hsv(:,:,1) <= 0.12;
+    h = hh | h;
+    s = hsv(:,:,2) >= smin+0.05 & hsv(:,:,2) <= smax;
     v = hsv(:,:,3) >= vmin & hsv(:,:,3) <= vmax;
-    
     result = h & s & v;
-    
 y = uint8(result)*255;  
